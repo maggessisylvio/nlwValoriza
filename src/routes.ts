708859224087fcbfa0f0d1284018b8a725bd842f
Sylvio Maggessi <sylviodev@gmail.com>
2021-06-24 17:49:54 -0300

    implementacao da entidade compliment

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { request, Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 
@@ -9,9 +10,11 @@ const router = Router();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/users", createUserController.handler);
 router.post("/tags", ensureAdmin, createTagController.handler); // middlewares é colocado diretamente na rota utilizada
 router.post("/login", authenticateUserController.handle);
+router.post("/compliments", createComplimentController.handle);
 
 export { router }
