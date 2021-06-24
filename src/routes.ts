066ff816f231be157d3c8bdb7081f03aff948da6
Sylvio Maggessi <sylviodev@gmail.com>
2021-06-24 13:29:46 -0300

    implementacao da entidade tag

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { request, Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post("/users", createUserController.handler);
+router.post("/tags", ensureAdmin, createTagController.handler); // middlewares é colocado diretamente na rota utilizada
 
 export { router }
